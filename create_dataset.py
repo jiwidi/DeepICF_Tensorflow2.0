41761e6d4baf0e7148f68da6c156b08ea2d2d57c
Jaime Ferrando <fhjaime96@gmail.com>
2019-05-21 20:06:15 +0200

    updated create dataset script and included data

--- create_dataset.py
+++ create_dataset.py
@@ -1,5 +1,4 @@
-# Author: Harshdeep Gupta
-# Date: 07 September, 2018
+# Based on code from  Harshdeep Gupta
 # Description: Splits the data into train and test using the leave the latest one out strategy 
 
 
@@ -7,9 +6,9 @@ import pandas as pd
 import numpy as np
 import os
 import requests, zipfile, io
+from random import sample 
 import logging
 from tqdm import tqdm
-import enlighten
 import argparse
 
 INPUT_PATH = 'data/ml-1m/'
@@ -17,7 +16,8 @@ INPUT_FILE = 'ratings.dat'
 
 OUTPUT_PATH_TRAIN = 'movielens.train.rating'
 OUTPUT_PATH_TEST = 'movielens.test.rating'
-OUTPUT_PATH_TEST_NEGATIVES = 'movielens.test.rating'
+OUTPUT_PATH_TEST_NEGATIVES = 'movielens.test.negative'
+OUTPUT_PATH_TRAIN_NEGATIVES = 'movielens.train.negative'
 USER_FIELD = 'userID'
 dataset_url = 'http://files.grouplens.org/datasets/movielens/ml-1m.zip'
 
@@ -29,10 +29,14 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Create dataset")
     parser.add_argument('--input_path', nargs='?', default=INPUT_PATH,
                         help='Input data path.')                    
-    parser.add_argument('--num_neg', type=int, default=4,
-                        help='Number of negative instances to pair with a positive instance.')
+    parser.add_argument('--num_neg_test', type=int, default=-1,
+                        help='Number of negative instances to pair with a positive instance for the test set. If -1 no negatives will be created')
+    parser.add_argument('--num_neg_train', type=int, default=-1,
+                        help='Number of negative instances to pair with a positive instance for the train set. If -1 no negatives will be created')
     parser.add_argument('--verbose', type=int, default=1,
                         help='Show info while running')
+    parser.add_argument('--force_download', type=bool, default=False,
+                        help='Forces the script to redownload the data')
     return parser.parse_args()
 
 
@@ -46,11 +50,11 @@ class TqdmLoggingHandler(logging.Handler):
         self.flush()
         
         
-def check_data(path):
+def check_data(path,force):
     '''
     Checks if the needed data files to create the dataset are inside the provided path
     '''
-    if(os.path.isfile(INPUT_PATH+INPUT_FILE)):
+    if(force or os.path.isfile(INPUT_PATH+INPUT_FILE)):
         return 0
     logging.info('Downloading file')
     oldhandlers = list(logger.handlers)
@@ -96,19 +100,94 @@ def get_train_test_df(transactions):
     test_df = test_df.sort_values(by=["userID", 'timestamp'])
     return train_df, test_df
 
+def get_test_negatives(transactions,negatives):
+    '''
+    return a negative sample dataframe, creates 4 negatives samples for every user positive rating.
+    Args:
+        transactions: the entire df of user/item transactions
+    '''
+    #Really slow, need to improve
+    users=[]
+    movies=[]
+    ratings=[]
+    list_movies = transactions.movieID.unique()
+    oldhandlers = list(logger.handlers)
+    logger.addHandler (TqdmLoggingHandler ())
+    logging.info('Creating negatives, this will take a while')
+    for user in tqdm(transactions.userID.unique()):
+        user_movies = transactions[transactions.userID == user].movieID.unique()
+        unseen_movies = [item for item in list_movies if item not in user_movies]
+        negative_movies = sample(unseen_movies,negatives)
+        for movie in negative_movies:
+            users.append(user)
+            movies.append(movie)
+            ratings.append(1)
+    negatives = pd.DataFrame({
+        "userID" : users,
+        "movieID" : movies,
+        "rating" : ratings}
+    )
+    logger.handlers = oldhandlers
+    return negatives
 
-def create_negatives():
-    ##To be done
-    return 0
-    
-def report_stats(transactions, train_df, test_df):
+def get_train_negatives(transactions,negatives):
+    '''
+    return a negative sample dataframe, creates 4 negatives samples for every user positive rating.
+    Args:
+        transactions: the entire df of user/item transactions
+    '''
+    #Really slow, need to improve
+    users=[]
+    movies=[]
+    ratings=[]
+    list_movies = transactions.movieID.unique()
+    oldhandlers = list(logger.handlers)
+    logger.addHandler (TqdmLoggingHandler ())
+    logging.info('Creating negatives, this will take a while')
+    for user in tqdm(transactions.userID):
+        user_movies = transactions[transactions.userID == user].movieID.unique()
+        unseen_movies = [item for item in list_movies if item not in user_movies]
+        negative_movies = sample(unseen_movies,negatives)
+        for movie in negative_movies:
+            users.append(user)
+            movies.append(movie)
+            ratings.append(1)
+    negatives = pd.DataFrame({
+        "userID" : users,
+        "movieID" : movies,
+        "rating" : ratings}
+    )
+    logger.handlers = oldhandlers
+    return negatives
+
+
+def report_stats(transactions, train, test, negative_test):
+    '''
+    return stats for a series of dataframes
+    Args:
+        transactions: the entire df of user/item transactions
+        train: Train dataframe
+        test: test dataframe
+        negative_test: negative_test dataframe
+    '''
     whole_size = transactions.shape[0]*1.0
-    train_size = train_df.shape[0]
-    test_size = test_df.shape[0]
-    logging.info("Total No. of Records = {}".format(whole_size))
-    logging.info("Train size = {}, Test size = {}".format(train_size, test_size))
-    logging.info("Train % = {}, Test % ={}".format(train_size/whole_size, test_size/whole_size))
+    train_size = train.shape[0]
+    test_size = test.shape[0]
+    negative_size = negative_test.shape[0]
+    print("Total No. of Records = {}".format(whole_size))
+    print("Train size = {}, Test size = {} Negative Test Size = {}".format(train_size, test_size, negative_size))
+    print("Train % = {}, Test % ={}".format(train_size/whole_size, test_size/whole_size))
 
+def create_mapping(values):
+    value_to_id = {value:idx for idx, value in enumerate(values.unique())}
+    return value_to_id
+
+def clean_df(transactions):
+    user_mapping = create_mapping(transactions["userID"])
+    item_mapping = create_mapping(transactions["movieID"])
+    transactions["userID"] = transactions["userID"].map(user_mapping.get)
+    transactions["movieID"] = transactions["movieID"].map(item_mapping.get)
+    return transactions
 
 def main():
     args = parse_args()
@@ -116,17 +195,29 @@ def main():
         logger.setLevel (logging.INFO)
     else:
         logger.setLevel (logging.WARNING)
-    check_data(INPUT_PATH)
+    check_data(INPUT_PATH,args.force_download)
     transactions = pd.read_csv(INPUT_PATH+INPUT_FILE, sep="::", names = ['userID', 'movieID', 'rating', 'timestamp'],engine='python')
-
+    transactions = clean_df(transactions)
     # convert to implicit scenario
     transactions['rating'] = 1
     
     # make the dataset
     train_df, test_df = get_train_test_df(transactions)
-    train_df.to_csv(INPUT_PATH+OUTPUT_PATH_TRAIN, header = False,index = False)
-    test_df.to_csv(INPUT_PATH+OUTPUT_PATH_TEST, header = False,index = False)
-    report_stats(transactions, train_df, test_df)
+    train_df.columns = [len(transactions.userID.unique()),len(transactions.movieID.unique()),0,0]
+    test_df.columns = [len(transactions.userID.unique()),len(transactions.movieID.unique()),0,0]
+    train_df.to_csv(INPUT_PATH+OUTPUT_PATH_TRAIN,index = False)
+    test_df.to_csv(INPUT_PATH+OUTPUT_PATH_TEST,index = False)
+    if(args.num_neg_test>0):
+        negative_test = get_test_negatives(transactions,args.num_neg_test)
+        negative_test.columns = [len(transactions.userID.unique()),len(transactions.movieID.unique()),0]
+        negative_test.to_csv(INPUT_PATH+OUTPUT_PATH_TEST_NEGATIVES,index = False)
+        report_stats(transactions, train_df, test_df,negative_test)
+    if(args.num_neg_train>0):
+        negative_train = get_train_negatives(transactions,args.num_neg_train)
+        negative_train.columns = [len(transactions.userID.unique()),len(transactions.movieID.unique()),0]
+        negative_train.to_csv(INPUT_PATH+OUTPUT_PATH_TRAIN_NEGATIVES,index = False)
+        report_stats(transactions, train_df, test_df,negative_test)
+    
     return 0
 
 
